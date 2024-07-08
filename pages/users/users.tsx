e2e3d787c7ebcbd401e561af858c985d3d16672e
Alankrit Khatri <alankritkhatri9@gmail.com>
2024-07-08 15:49:37 +0530

    refactored the datatable code to make it a component with data flowing from a db/data file

--- pages/users/users.tsx
+++ pages/users/users.tsx
@@ -1,12 +1,74 @@
+import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../src/components/dataTable/DataTable";
 import "./users.css";
+import { userRows } from "../home/data";
+const columns: GridColDef<(typeof userRows)[number]>[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "profilePicture",
+    headerName: "Profile Picture",
+    width: 150,
+    editable: true,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "actions",
+    headerName: "Actions",
+    width: 100,
+    renderCell: (_) => {
+      return (
+        <div className="action">
+          <div className="view">View</div>
+          <div className="delete">Delete</div>
+        </div>
+      );
+    },
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 100,
+    type: "boolean",
+  },
+
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (_, row) => `${row.firstName || ""} ${row.lastName || ""}`,
+  },
+];
+
 const Users = () => {
   return (
     <div className="users">
       <div className="info">
         <h1>users</h1>
         <button>Add New Users</button>
-        <DataTable />
+        <DataTable columns={columns} rows={userRows} />
       </div>
     </div>
   );
--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,90 +1,21 @@
 import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import "./dataTable.css";
-const DataTable = () => {
-  const columns: GridColDef<(typeof rows)[number]>[] = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "profilePicture",
-      headerName: "Profile Picture",
-      width: 150,
-      editable: true,
-      renderCell: (params) => {
-        return <img src={params.row.img || "/noavatar.png"} alt="" />;
-      },
-    },
-    {
-      field: "actions",
-      headerName: "Actions",
-      width: 150,
+type Props = {
+  columns: GridColDef[];
 
-      editable: true,
-      renderCell: (_) => {
-        return (
-          <div className="action">
-            <div className="view">View</div>
-            <div className="delete">Delete</div>
-          </div>
-        );
-      },
-    },
-    {
-      field: "status",
-      headerName: "Status",
-      width: 100,
-      type: "boolean",
-    },
-
-    {
-      field: "firstName",
-      headerName: "First name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "lastName",
-      headerName: "Last name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 110,
-      editable: true,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (_, row) => `${row.firstName || ""} ${row.lastName || ""}`,
-    },
-  ];
-
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 14, status: true },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  ];
-
+  rows: object[];
+};
+const DataTable = (props: Props) => {
   return (
     <div className="dataTable">
       <DataGrid
         className="dataGrid"
-        rows={rows}
-        columns={columns}
+        rows={props.rows}
+        columns={props.columns}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 10,
             },
           },
         }}
