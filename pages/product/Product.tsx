997fb1d653eeb61276d056b275541092907d1522
Alankrit Khatri <alankritkhatri9@gmail.com>
2024-07-29 13:31:51 +0530

    fixed bug in the produc.tsx file

--- pages/product/Product.tsx
+++ pages/product/Product.tsx
@@ -7,48 +7,50 @@ const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
   {
     field: "img",
-    headerName: "Avatar",
-    width: 150,
-    editable: true,
+    headerName: "Image",
+    width: 100,
     renderCell: (params) => {
       return <img src={params.row.img || "/noavatar.png"} alt="" />;
     },
   },
   {
-    field: "status",
-    headerName: "Status",
-    width: 100,
+    field: "title",
+    type: "string",
+    headerName: "Title",
+    width: 250,
+  },
+  {
+    field: "color",
+    type: "string",
+    headerName: "Color",
+    width: 150,
+  },
+  {
+    field: "price",
+    type: "string",
+    headerName: "Price",
+    width: 200,
+  },
+  {
+    field: "producer",
+    headerName: "Producer",
+    type: "string",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "inStock",
+    headerName: "In Stock",
+    width: 150,
     type: "boolean",
   },
-
-  {
-    field: "firstName",
-    headerName: "First name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "lastName",
-    headerName: "Last name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 110,
-    editable: true,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (_, row) => `${row.firstName || ""} ${row.lastName || ""}`,
-  },
 ];
+
 const Product = () => {
   const [open, setOpen] = useState(false);
 
