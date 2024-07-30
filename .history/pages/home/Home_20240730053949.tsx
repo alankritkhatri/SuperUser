402ba85eff8ef4c337c852909e09a31f821e6381
Alankrit Khatri <alankritkhatri9@gmail.com>
2024-07-30 16:31:14 +0530

    made site responsive

---
+++ .history/pages/home/Home_20240730053949.tsx
@@ -0,0 +1,47 @@
+import "./Home.css";
+import TopBox from "../../src/components/topBox/TopBox";
+import ChartBox from "../../src/components/ChartBox.ts/ChartBox";
+import {
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+  barChartBoxRevenue,
+  barChartBoxVisit,
+} from "../../src/data";
+import BarChartBox from "../../src/components/BarChartBox/BarChartBox";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box4">Box 4</div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        {" "}
+        <ChartBox {...chartBoxRevenue} />
+      </div>
+      <div className="box box7"></div>
+      <div className="box box8">
+        {" "}
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box9">
+        {" "}
+        <BarChartBox {...barChartBoxRevenue} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
