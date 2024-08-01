32e05fdfbf4c01340eeab3c29f6a4d5c63bb2ce6
Alankrit Khatri <alankritkhatri9@gmail.com>
2024-08-01 08:12:09 +0530

    landing page design

---
+++ .history/pages/LandingPage/LandingPage_20240801080941.tsx
@@ -0,0 +1,22 @@
+import "./LandingPage.css";
+import Navbar from "../../src/components/navbar/Navbar";
+import Footer from "../../src/components/footer/footer";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page ">
+      <div className="bg"> </div>
+      <Navbar />
+      <div className="landing-text">
+        <div className="copyline">
+          <h1>Your Last User,Revenue and Invoicing Dashboard</h1>
+        </div>
+        <img className="scnshot " src="/screenzy-1722425437701.png" alt="" />
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
